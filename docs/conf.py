8b08a4eca2a15f3f32eeee7d6d5b80036c39325a
Eric Pierce <wapcaplet88@gmail.com>
2011-01-30 21:29:41 -0700

    Misc. rtd.css revisions (going obsolete, now that it's live on RTD). Back-tagged 0.1 version.

--- docs/conf.py
+++ docs/conf.py
@@ -94,7 +94,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  Major themes that come with
 # Sphinx are currently 'default' and 'sphinxdoc'.
-html_theme = 'default'
+html_theme = 'sphinxdoc'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -124,7 +124,7 @@ html_title = "CSVSee documentation"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-html_style = 'rtd.css'
+#html_style = 'custom.css'
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
